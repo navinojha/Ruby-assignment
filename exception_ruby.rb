de0d6e91e83ee41d08dcf0282d2a188c42a98026
nojha <nojha@C02DW8HWMD6M.local>
2021-01-24 16:53:19 +0530

    Singleton_methods.rb

--- exception_ruby.rb
+++ exception_ruby.rb
@@ -1,21 +1,21 @@
-# class DivisionError
-#     def initialize(n)
-#         @num = n
-#     end
+class DivisionError
+    def initialize(n)
+        @num = n
+    end
 
-#     def divide_by_zero
-#         begin
-#         @num/0
-#         rescue StandardError => e
-#             puts "It will be infinity"
-#         else
-#             puts "No exception raised"
-#         end
-#     end
-# end
+    def divide_by_zero
+        begin
+        @num/0
+        rescue StandardError => e
+            puts "It will be infinity"
+        else
+            puts "No exception raised"
+        end
+    end
+end
 
-# exception = DivisionError.new(10)
-# p exception.divide_by_zero
+exception = DivisionError.new(10)
+p exception.divide_by_zero
 
 # Method 2
 
---
+++ singleton_methods.rb
@@ -0,0 +1,10 @@
+obj = Array.new
+
+def obj.size
+  puts "0"
+end
+
+obj = [1,2,3,4]
+puts obj.size
+
+obj.size
