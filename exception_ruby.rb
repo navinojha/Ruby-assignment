76c338bc21af3c27612809bc183c1c07ac73c606
nojha <nojha@C02DW8HWMD6M.local>
2021-01-24 15:49:00 +0530

    Exceptio_Ruby_Program

---
+++ exception_ruby.rb
@@ -0,0 +1,37 @@
+# class DivisionError
+#     def initialize(n)
+#         @num = n
+#     end
+
+#     def divide_by_zero
+#         begin
+#         @num/0
+#         rescue StandardError => e
+#             puts "It will be infinity"
+#         else
+#             puts "No exception raised"
+#         end
+#     end
+# end
+
+# exception = DivisionError.new(10)
+# p exception.divide_by_zero
+
+# Method 2
+
+class DivisionError<StandardError
+    def initialize(n)
+      @num=n
+    end
+  
+    def divide_by_zero
+        begin
+            @num/0
+        rescue Exception => e
+            raise DivisionError.new("Divide by Zero Error") 
+        end
+    end
+end
+
+exception = DivisionError.new(10)
+p exception.divide_by_zero
