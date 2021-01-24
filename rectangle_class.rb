fa7e13eed591fe638787cec7d9619a84e6cffc52
nojha <nojha@C02DW8HWMD6M.local>
2021-01-24 10:57:44 +0530

    Updated rectangle_class.rb

--- rectangle_class.rb
+++ rectangle_class.rb
@@ -12,6 +12,12 @@ class Rectangle
 end
 
 rectangle1 = Rectangle.new(10,20)
-rectangle1.length = 10 // set Length
-rectangle1.breadth = 20 //set breadth
-puts rectangle1.getArea // get area
+rectangle1.length = 10 # set Length
+rectangle1.breadth = 20 #set breadth
+puts rectangle1.getArea # get area
+
+rectangle2 = Rectangle.new(20,30)
+rectangle2.length = 20 # set Length
+rectangle2.breadth = 30 #set breadth
+puts rectangle2.getArea # get area
+
