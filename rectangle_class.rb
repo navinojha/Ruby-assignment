d2f97b1bc27cea7a6df63b8c9c806fa185036179
nojha <nojha@C02DW8HWMD6M.local>
2021-01-24 10:44:38 +0530

    Rectangle Class

---
+++ rectangle_class.rb
@@ -0,0 +1,17 @@
+class Rectangle
+    attr_reader :area 
+    attr_writer :length, :breadth
+
+    def initialize(l,b)
+        @length, @breadth = l, b
+    end
+
+    def getArea
+        @area = @length*@breadth
+    end
+end
+
+rectangle1 = Rectangle.new(10,20)
+rectangle1.length = 10 // set Length
+rectangle1.breadth = 20 //set breadth
+puts rectangle1.getArea // get area
