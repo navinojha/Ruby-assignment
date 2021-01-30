62151efb80c7fa3dd7415c7358326bbca51b38df
nojha <nojha@C02DW8HWMD6M.local>
2021-01-30 12:18:43 +0530

    Implementation of map function

--- lambda_multiply_two_numbers.rb
+++ lambda_multiply_two_numbers.rb
@@ -1,7 +1,10 @@
 
-def multiply_two_numbers(m_lamda)
-    m_lamda.call
-end
+# def multiply_two_numbers(m_lamda)
+#     m_lamda.call
+# end
 
- multiply_lamda = -> { 2* 4}
- p  multiply_two_numbers(multiply_lamda) # output 8
+#  multiply_lamda = -> { 2* 4}
+#  p  multiply_two_numbers(multiply_lamda) # output 8
+
+array = [100, 200, 300, 400, 500] 
+p array[5]
---
+++ ruby_map_examples.rb
@@ -0,0 +1,27 @@
+
+# Implementation of map function
+class Array
+    def maap()
+        output = []
+        if block_given?
+            self.each { |e| output << yield(e) }
+        else
+            output = to_enum :maap
+        end
+        output
+    end
+end
+
+p [1,2,3,4].to_enum
+
+[1,2,3].maap { |num| puts num*2}
+
+
+# class Array
+#     def map!
+#         return to_enum :map! unless block_given?
+#         self.each_with_index { |e, index| }
+#     end
+# end
+
+# [1,2,3].map { |num| puts num*2}
